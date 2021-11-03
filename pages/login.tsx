22eed6c176e9ac3617ff67244133c43fee980275
darkhappy <darkhappyy@gmail.com>
2021-11-02 20:18:33 -0400

    auth: Added DA checker

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,16 @@
-import { auth, googleAuthProvider } from "../lib/firebase";
+import { auth, firestore, googleAuthProvider } from "../lib/firebase";
 import { signInWithPopup, signOut } from "@firebase/auth";
-import { Button, Container } from "react-bootstrap";
-import { useContext } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import React, {
+	ChangeEvent,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
 import { UserContext } from "../lib/context";
+import { doc, getDoc } from "@firebase/firestore";
+import { debounce } from "lodash";
 
 export default function Enter() {
 	const { user, da } = useContext(UserContext);
@@ -56,5 +64,66 @@ function SignOutButton() {
 }
 
 function UsernameForm() {
-	return null;
+	const [formValue, setFormValue] = useState("");
+	const [isValid, setIsValid] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const { user, da } = useContext(UserContext);
+
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const val = e.target.value.toLowerCase();
+		const re = /^[0-9]{7}$/;
+
+		if (val.length != 7) {
+			setFormValue(val);
+			setLoading(false);
+			setIsValid(false);
+		}
+
+		if (re.test(val)) {
+			setFormValue(val);
+			setLoading(true);
+			setIsValid(false);
+		}
+	};
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const checkDA = useCallback(
+		debounce(async (da) => {
+			if (da.length == 7) {
+				const ref = doc(firestore, "das/", da);
+				setIsValid(!(await getDoc(ref)).exists());
+				setLoading(false);
+			}
+		}, 500),
+		[]
+	);
+
+	useEffect(() => {
+		checkDA(formValue);
+	}, [checkDA, formValue]);
+
+	return (
+		<section>
+			<h3>Quelques informations</h3>
+			<Form>
+				<Form.Group className="mb-3" controlId="formBasicUsername">
+					<Form.Label>DA</Form.Label>
+					<Form.Control
+						type="number"
+						placeholder="1234567"
+						value={formValue}
+						onChange={onChange}
+					/>
+					<Form.Text>
+						Le DA sert comme clé unique. Vous pouvez mettre un nom d{"'"}
+						utilisateur personalisé plus tard.
+					</Form.Text>
+				</Form.Group>
+				<Button type="submit" variant="primary" disabled={!isValid}>
+					Choose
+				</Button>
+			</Form>
+		</section>
+	);
 }
